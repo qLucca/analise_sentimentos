1ae389bd6b577f487e150ebd406c3dc054ed80d3
qLucca <luccaq01@outlook.com>
2026-06-22 20:05:36 -0300

    Fix MySQL load pipeline input paths

--- src/pipelines/run_load_mysql.py
+++ src/pipelines/run_load_mysql.py
@@ -4,14 +4,17 @@ import pandas as pd
 
 from src.database.ingestion import load_gold_dataframe, load_silver_reviews
 from src.utils.logger import setup_logging
-from src.utils.paths import PROCESSED_DIR
+from src.utils.paths import (
+    GOLD_DASHBOARD_PRIMARY_MODEL_DATASET_PATH,
+    SILVER_UNIFIED_DATASET_PATH,
+)
 
 
 def run() -> None:
     setup_logging()
 
-    silver = pd.read_csv(PROCESSED_DIR / "textual_dataset_preprocessed.csv")
-    gold = pd.read_csv(PROCESSED_DIR / "youtube_with_predicted_sentiment_bertimbau.csv")
+    silver = pd.read_csv(SILVER_UNIFIED_DATASET_PATH)
+    gold = pd.read_csv(GOLD_DASHBOARD_PRIMARY_MODEL_DATASET_PATH)
 
     load_silver_reviews(silver, if_exists="replace")
     load_gold_dataframe(gold, "sentiment_analysis", if_exists="replace")
